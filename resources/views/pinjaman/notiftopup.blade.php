5e356ed0853ce3f097eea8963e651db04e517a06
Adam Adifa <adamabdi.al.a@gmail.com>
2023-05-15 21:58:54 +0700

    update

--- resources/views/pinjaman/notiftopup.blade.php
+++ resources/views/pinjaman/notiftopup.blade.php
@@ -127,9 +127,29 @@ function rupiah($nilai)
 @section('content')
 <div class="row" style="margin-top: 70px">
     <div class="col">
-        <div class="alert alert-primary" role="alert">
-            <h4 class="alert-title">Title</h4>
-            Default alert with title.
+        <div class="alert alert-danger" role="alert">
+            <h4 class="alert-title">Warning</h4>
+            <p>Karyawan Tidak Dapat Mengajukan Pinjaman Karena Masih Memiliki Pinjaman Yang Belum Lunas, Untuk Melakukan Pinjaman Kembali Min. Sudah Membayar 75% dari Total Pinjaman Sebelumnya
+                <br>
+                <table class="table">
+                    <tr>
+                        <th>No. Pinjaman</th>
+                        <td>{{ $cekpinjaman->no_pinjaman }}</td>
+                    </tr>
+                    <tr>
+                        <th>Tanggal</th>
+                        <td>{{ $cekpinjaman->tgl_pinjaman }}</td>
+                    </tr>
+                    <tr>
+                        <th>Jumlah Pinjaman</th>
+                        <td style="text-align:right">{{ rupiah($cekpinjaman->jumlah_pinjaman) }}</td>
+                    </tr>
+                    <tr>
+                        <th>Total Pembayaran</th>
+                        <td style="text-align:right">{{ rupiah($cekpinjaman->totalpembayaran) }} ({{ $cekpinjaman->totalpembayaran / $cekpinjaman->jumlah_pinjaman * 100 }}%) </td>
+                    </tr>
+                </table>
+            </p>
         </div>
     </div>
 </div>
