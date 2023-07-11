7a61e45aa28b14d6d4d73a14117a1c7c8a02aca8
Adam Adifa <adamabdi.al.a@gmail.com>
2023-07-12 00:08:30 +0700

    update

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -1151,7 +1151,7 @@ class PresensiController extends Controller
             if (!empty($last_lintashari) && $tgl_pulang_shift_3 <= "10:00") {
                 $tgl_presensi = $lastday;
                 $tgl_pulang = date('Y-m-d', strtotime('+1 day', strtotime($tgl_presensi)));
-                $jam_pulang = $tgl_pulang . " " . $ceklastpresensi->jam_pulang;
+                $jam_pulang = $tgl_pulang . " " . date("H:i", strtotime($ceklastpresensi->jam_pulang));
             } else {
                 if ($tgl_pulang_shift_3 <= "08:00") {
                     $tgl_presensi = $lastday;
@@ -1161,7 +1161,7 @@ class PresensiController extends Controller
                     $kode_jadwal = "JD004";
                 } else {
                     $tgl_pulang = $tgl_presensi;
-                    $jam_pulang = $tgl_pulang . " " . $jam_kerja->jam_pulang;
+                    $jam_pulang = $tgl_pulang . " " . date("H:i", strtotime($jam_kerja->jam_pulang));
                 }
                 // $tgl_pulang = $tgl_presensi;
                 // $jam_pulang = $tgl_pulang . " " . $jam_kerja->jam_pulang;
