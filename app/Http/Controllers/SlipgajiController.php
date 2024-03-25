731e2e28638ee44dce2f7ee293fb2c1c24069347
Adam Adifa <adamabdi.al.a@gmail.com>
2024-03-25 13:36:17 +0700

    update

--- app/Http/Controllers/SlipgajiController.php
+++ app/Http/Controllers/SlipgajiController.php
@@ -292,12 +292,54 @@ class SlipgajiController extends Controller
 
     public function cetakthr($bulan, $tahun)
     {
+
+        $bl = $bulan;
+        $kode_potongan = "GJ" . $bulan . $tahun;
+        if ($bulan == 1) {
+            $lastbulan = 12;
+            $lasttahun = $tahun - 1; //2023
+        } else {
+            $lastbulan = $bulan - 1;
+            $lasttahun = $tahun;
+        }
+
+        if ($bulan == 12) {
+            $nextbulan = 1;
+            $nexttahun = $tahun + 1;
+        } else {
+            $nextbulan = $bulan + 1;
+            $nexttahun = $tahun;
+        }
+        $lastbulan = $lastbulan < 10 ?  "0" . $lastbulan : $lastbulan;
+        $bulan = $bulan < 10 ?  "0" . $bulan : $bulan;
+
+        $dari = $lasttahun . "-" . $lastbulan . "-21";
+        $sampai = $tahun . "-" . $bulan . "-20";
+
+
+
+        $daribulangaji = $dari;
+        $berlakugaji = $sampai;
         $akhir_periode = $tahun . "-" . $bulan . "-01";
         $karyawan = DB::table('master_karyawan')
+            ->selectRaw('master_karyawan.*,gaji_pokok,t_jabatan,t_masakerja,t_tanggungjawab,
+             t_makan,t_istri,t_skill,nama_dept,nama_jabatan,nama_cabang')
             ->join('cabang', 'master_karyawan.id_kantor', '=', 'cabang.kode_cabang')
             ->join('hrd_departemen', 'master_karyawan.kode_dept', '=', 'hrd_departemen.kode_dept')
             ->join('hrd_jabatan', 'master_karyawan.id_jabatan', '=', 'hrd_jabatan.id')
-            ->where('nik', Auth::guard('karyawan')->user()->nik)->first();
+            ->leftJoin(
+                DB::raw("(
+                        SELECT nik,gaji_pokok,t_jabatan,t_masakerja,t_tanggungjawab,
+                        t_makan,t_istri,t_skill
+                        FROM hrd_mastergaji
+                        WHERE kode_gaji IN (SELECT MAX(kode_gaji) as kode_gaji FROM hrd_mastergaji
+                        WHERE tgl_berlaku <= '$berlakugaji'  GROUP BY nik)
+                    ) hrdgaji"),
+                function ($join) {
+                    $join->on('master_karyawan.nik', '=', 'hrdgaji.nik');
+                }
+            )
+            ->where('master_karyawan.nik', Auth::guard('karyawan')->user()->nik)->first();
         return view('slipgaji.cetak_thr', compact('tahun', 'karyawan'));
     }
 }
--- resources/views/slipgaji/cetak_thr.blade.php
+++ resources/views/slipgaji/cetak_thr.blade.php
@@ -10,8 +10,8 @@
         }
 
         /* .historicontent {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            justify-content: left !important;
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        } */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    justify-content: left !important;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                } */
     </style>
     <!-- App Header -->
     <div class="appHeader bg-primary text-light">
@@ -26,6 +26,44 @@
     <!-- * App Header -->
 @endsection
 @section('content')
+    @php
+        $upah =
+            $karyawan->gaji_pokok +
+            $karyawan->t_jabatan +
+            $karyawan->t_masakerja +
+            $karyawan->t_tanggungjawab +
+            $karyawan->t_makan +
+            $karyawan->t_istri +
+            $karyawan->t_skill;
+        //Masa Kerja THR
+        $sampaithr = '2024-03-23';
+        $awal = date_create($karyawan->tgl_masuk);
+        $akhir = date_create($sampaithr); // waktu sekarang
+        $diff = date_diff($awal, $akhir);
+        // echo $diff->y . ' tahun, '.$diff->m.' bulan, '.$diff->d.' Hari'
+        $tahunkerja = $diff->y;
+        $bulankerja = $diff->m;
+    @endphp
+    @if ($tahunkerja >= 10 && $tahunkerja < 15)
+        @php
+            $thr2 = 0.25 * $karyawan->gaji_pokok;
+        @endphp
+    @else
+        @php
+            $thr2 = 0;
+        @endphp
+    @endif
+
+    @if ($tahunkerja >= 15)
+        @php
+            $thr3 = 0.5 * $karyawan->gaji_pokok;
+        @endphp
+    @else
+        @php
+            $thr3 = 0;
+        @endphp
+    @endif
+
     <div class="row" style="margin-top: 70px; overflow:scroll; height:100%; position:relative; bottom:10%">
         <div class="col">
             <table class="table">
@@ -48,6 +86,89 @@
                 <tr>
                     <th>Jabatan</th>
                     <td>{{ $karyawan->nama_jabatan }}</td>
+                </tr>
+                <tr>
+                    <th>Masa Kerja</th>
+                    <td>
+                        @php
+                            echo $diff->y . ' tahun, ' . $diff->m . ' bulan';
+                        @endphp
+                    </td>
+                </tr>
+
+
+            </table>
+            <hr>
+            <table class="table">
+                <tr>
+                    <th>Gaji Pokok</th>
+                    <td class="text-right">{{ rupiah($karyawan->gaji_pokok) }}</td>
+                </tr>
+                <tr>
+                    <th>Tunj. Jabatan</th>
+                    <td class="text-right">{{ rupiah($karyawan->t_jabatan) }}</td>
+                </tr>
+                <tr>
+                    <th>Tunj. Masa Kerja</th>
+                    <td class="text-right">{{ rupiah($karyawan->t_masakerja) }}</td>
+                </tr>
+                <tr>
+                    <th>Tunj. Tanggung Jawab</th>
+                    <td class="text-right">{{ rupiah($karyawan->t_tanggungjawab) }}</td>
+                </tr>
+                <tr>
+                    <th>Tunj. Makan</th>
+                    <td class="text-right">{{ rupiah($karyawan->t_makan) }}</td>
+                </tr>
+
+                @if (!empty($karyawan->t_istri))
+                    <tr>
+                        <th>Tunj. Istri</th>
+                        <td class="text-right">{{ rupiah($karyawan->t_istri) }}</td>
+                    </tr>
+                @endif
+
+                <tr>
+                    <th>Tunj. Skill</th>
+                    <td class="text-right">{{ rupiah($karyawan->t_skill) }}</td>
+                </tr>
+                <tr>
+                    <th>THR</th>
+                    <td align="right">
+                        @if ($tahunkerja >= 1)
+                            @php
+                                $thr = $upah;
+                            @endphp
+                        @else
+                            @php
+                                $thr = ($bulankerja / 12) * $upah;
+                            @endphp
+                        @endif
+                        {{ rupiah($thr) }}
+                    </td>
+                </tr>
+                @if (!empty($thr2))
+                    <tr>
+                        <th>THR 1/4</th>
+                        <td align="right">
+                            {{ rupiah($thr2) }}
+                        </td>
+                    </tr>
+                @endif
+
+                @if (!empty($thr3))
+                    <tr>
+                        <th>THR 1/2</th>
+                        <td align="right">
+                            {{ rupiah($thr3) }}
+                        </td>
+                    </tr>
+                @endif
+                <tr>
+                    <th>TOTAL THR</th>
+                    <th style="text-align:right; font-size:16px">
+                        {{ rupiah($thr + $thr2 + $thr3) }}
+                    </th>
                 </tr>
             </table>
         </div>
