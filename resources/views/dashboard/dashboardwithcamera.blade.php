6402b539533e7c3564ea63e085659341f9593fc7
Adam Adifa <adamabdi.al.a@gmail.com>
2024-11-01 08:46:39 +0700

    update

--- resources/views/dashboard/dashboardwithcamera.blade.php
+++ resources/views/dashboard/dashboardwithcamera.blade.php
@@ -325,6 +325,7 @@
                                                                         $jam_keluar,
                                                                         $jam_kembali,
                                                                         $jam_selesai,
+                                                                        $jam_out,
                                                                         $d->total_jam,
                                                                         $d->istirahat,
                                                                         $jam_awal_istirahat,
