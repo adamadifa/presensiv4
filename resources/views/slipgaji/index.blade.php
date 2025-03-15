2f5bef505b181593e7b4a21872239517a9722d79
Adam Adifa <adamabdi.al.a@gmail.com>
2025-03-15 09:09:33 +0700

    update

--- resources/views/slipgaji/index.blade.php
+++ resources/views/slipgaji/index.blade.php
@@ -10,8 +10,8 @@
         }
 
         /* .historicontent {
-                                                                                                                                                                                                            justify-content: left !important;
-                                                                                                                                                                                                        } */
+                                                                                                                                                                                                                        justify-content: left !important;
+                                                                                                                                                                                                                    } */
     </style>
     <!-- App Header -->
     <div class="appHeader bg-primary text-light">
@@ -137,6 +137,32 @@
                         </div>
                     </div>
                 </a>
+                @if ($d->bulan == '2' && $tahun == '2025')
+                    <a href="/slipgaji/3/2025/cetakthr">
+                        <div class="row mb-1">
+                            <div class="col">
+                                <div class="card historiborderred">
+                                    <div class="card-body">
+                                        <div class="historicontent">
+                                            <div class="historidetail1">
+                                                <div class="iconpresence">
+                                                    <ion-icon name="document-text-outline" style="font-size: 64px;" class="text-danger"></ion-icon>
+                                                </div>
+                                                <div class="datepresence">
+                                                    <h4>THR {{ $d->tahun }}</h4>
+
+                                                    <small class="text-muted">
+                                                        THR 2025
+                                                    </small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @endif
             @endforeach
 
 
